91dd45762976113f98106f5d406b4f906ee269a6
Aditya Ayatusy <adityaayatusy@gmail.com>
2026-08-06 14:23:13 +0700

    style(test): tidy up Python and Go test scripts in test folder

--- test/token_num.py
+++ test/token_num.py
@@ -1,65 +1,55 @@
+import os
 import tiktoken as tk
 
 def read_data_from_file(filename):
     """
-    Read data from file
+    Read data from test file
     :param filename: file name
     :return: text_list, model_list, encoding_list
     """
+    if not os.path.exists(filename):
+        filename = "test.txt"
     with open(filename, 'r', encoding='utf-8') as f:
-        data = f.read()
-        text_list = data.splitlines()[0].split(',')
-        model_list = data.splitlines()[1].split(',')
-        encoding_list = data.splitlines()[2].split(',')
+        lines = f.read().splitlines()
+        text_list = lines[0].split(',')
+        model_list = lines[1].split(',')
+        encoding_list = lines[2].split(',')
     
     return text_list, model_list, encoding_list
 
 def get_token_by_model(text, model):
     """
-    Get token by model
-    :param text: text
-    :param model: model
-    :return: num_tokens
+    Get token count by model
     """
-    encoding = tk.encoding_for_model(model)
-    num_tokens = len(encoding.encode(text))
-    return num_tokens
+    try:
+        encoding = tk.encoding_for_model(model)
+        return len(encoding.encode(text))
+    except Exception as e:
+        print(f"Error model {model}: {e}")
+        return 0
 
 def get_token_by_encoding(text, encoding_name):
     """
-    Get token by encoding
-    :param text: text
-    :param encoding: encoding
-    :return: num_tokens
+    Get token count by encoding name
     """
-    encoding = tk.get_encoding(encoding_name)
-    num_tokens = len(encoding.encode(text))
-    return num_tokens
-
+    try:
+        encoding = tk.get_encoding(encoding_name)
+        return len(encoding.encode(text))
+    except Exception as e:
+        print(f"Error encoding {encoding_name}: {e}")
+        return 0
 
 def test_token_by_model(text_list, model_list):
-    """
-    Test token by model
-    :param text_list: text list
-    :param model_list: model list
-    :return: None
-    """
     for text in text_list:
         for model in model_list:
             num_tokens = get_token_by_model(text, model)
-            print('text: {}, model: {}, token: {}'.format(text, model, num_tokens))
+            print(f"text: {text}, model: {model}, token: {num_tokens}")
 
 def test_token_by_encoding(text_list, encoding_list):
-    """
-    Test token by encoding
-    :param text_list: text list
-    :param encoding_list: encoding list
-    :return: None
-    """
     for text in text_list:
         for encoding in encoding_list:
             num_tokens = get_token_by_encoding(text, encoding)
-            print('text: {}, encoding: {}, token: {}'.format(text, encoding, num_tokens))
+            print(f"text: {text}, encoding: {encoding}, token: {num_tokens}")
 
 if __name__ == '__main__':
     text_list, model_list, encoding_list = read_data_from_file('test/test.txt')
